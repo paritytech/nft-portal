952b7ae86a840d5f7baf6a4e7e2725a110040a6a
saltycucumber <vadim.sav@gmail.com>
2022-12-20 22:04:35 +0200

    moved api keys to netlify

--- src/api/pinata.ts
+++ src/api/pinata.ts
@@ -1,25 +1,17 @@
-import axios from 'axios';
-
 import { CollectionMetadataData } from '@helpers/interfaces';
 
-// TODO will need to use netlify or something else in order to not expose API keys
 export const saveToIpfs = async (metadata: CollectionMetadataData): Promise<string> => {
   const data = JSON.stringify({
     pinataContent: metadata,
   });
 
-  const call = {
+  const response = await fetch('/.netlify/functions/pinata', {
     method: 'post',
-    url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
     headers: {
       'Content-Type': 'application/json',
-      pinata_api_key: '%PINATA_KEY%', // do not push actual key to github
-      pinata_secret_api_key: '%PINATA_API_KEY%', // do not push actual key to github
     },
-    data,
-  };
+    body: data,
+  }).then((res) => res.json());
 
-  const res = await axios(call);
-
-  return res.data.IpfsHash;
+  return response.ipfsHash;
 };
