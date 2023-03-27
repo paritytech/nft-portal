85d479709d2fd295429f6db76f5a125e45f4bca3
saltycucumber <vadim.sav@gmail.com>
2023-03-27 17:04:27 +0300

    extract constant

--- src/helpers/routes.ts
+++ src/helpers/routes.ts
@@ -1,3 +1,6 @@
+const COLLECTION_ID_PARAM = ':collectionId';
+const NFT_ID_PARAM = ':collectionId';
+
 export const routes = {
   homepage: '/discover',
 
@@ -5,10 +8,10 @@ export const routes = {
     index: '/my-assets',
     collections: '/my-assets/collections',
     collectionMint: '/my-assets/collections/mint',
-    collectionEdit: (collectionId: string = ':collectionId') => `/my-assets/collections/edit/${collectionId}`,
-    nfts: (collectionId: string = ':collectionId') => `/my-assets/collections/${collectionId}/nfts`,
-    nftMint: (collectionId: string = ':collectionId') => `/my-assets/collections/${collectionId}/nfts/mint`,
-    nftEdit: (collectionId: string = ':collectionId', nftId: string = ':nftId') =>
+    collectionEdit: (collectionId: string = COLLECTION_ID_PARAM) => `/my-assets/collections/edit/${collectionId}`,
+    nfts: (collectionId: string = COLLECTION_ID_PARAM) => `/my-assets/collections/${collectionId}/nfts`,
+    nftMint: (collectionId: string = COLLECTION_ID_PARAM) => `/my-assets/collections/${collectionId}/nfts/mint`,
+    nftEdit: (collectionId: string = COLLECTION_ID_PARAM, nftId: string = NFT_ID_PARAM) =>
       `/my-assets/collections/${collectionId}/nfts/edit/${nftId}`,
   },
 
