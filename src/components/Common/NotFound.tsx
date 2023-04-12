9712d45d4c933736f8867a8067250c4003a1ef14
Jegor Sidorenko <jegor@parity.io>
2023-04-12 16:09:17 +0300

    Add TODO

--- src/components/Common/NotFound.tsx
+++ src/components/Common/NotFound.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 
+// TODO: tell designer to create a 404 template
 const NotFound = () => {
   return <div style={{ textAlign: 'center', paddingTop: '50px' }}>Page not found</div>;
 };
