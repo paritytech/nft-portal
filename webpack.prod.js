80b956659e5cd11eda48ab63ba659ff3c8f55dbf
saltycucumber <vadim.sav@gmail.com>
2023-05-11 20:09:54 +0300

    prod webpack build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,11 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { merge } from 'webpack-merge';
+
+import common from './webpack.common.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default merge(common, {
   mode: 'production',
