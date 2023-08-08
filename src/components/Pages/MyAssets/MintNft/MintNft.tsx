e11a89152b1006f5ff86b3226eb55b81f8bcfd49
Ben Greenberg <bengreenberg@gmail.com>
2023-08-08 13:49:53 -0400

    add recipient wallet qr code scanner

--- src/components/Pages/MyAssets/MintNft/MintNft.tsx
+++ src/components/Pages/MyAssets/MintNft/MintNft.tsx
@@ -2,24 +2,20 @@ import { BN } from '@polkadot/util';
 import { ChangeEvent, FormEvent, memo, useCallback, useEffect, useRef, useState } from 'react';
 import { FormControl, Stack } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
+import { useZxing } from "react-zxing";
 
 import { saveImageToIpfs } from '@api/pinata.ts';
-
 import ActionButton from '@buttons/ActionButton.tsx';
-
 import FileDropZone from '@common/FileDropZone.tsx';
 import ModalStatus from '@common/ModalStatus.tsx';
 import Radio from '@common/Radio.tsx';
 import ShowRestrictionMessage from '@common/ShowRestrictionMessage.tsx';
-
 import { useAccounts } from '@contexts/AccountsContext.tsx';
-
 import { RestrictionTypes } from '@helpers/constants.ts';
 import { CollectionConfigJson, CollectionMetadataData, MintAccessNft } from '@helpers/interfaces.ts';
 import { SFormBlock, SInfoRow, SPageControls } from '@helpers/reusableStyles.ts';
 import { SFormLayout, SGroup, SLabel } from '@helpers/styledComponents.ts';
 import { generateNftId, getCleanFormattedBalance } from '@helpers/utilities.ts';
-
 import { useCheckMintingEligibility } from '@hooks/useCheckMintingEligibility.ts';
 import { useCollections } from '@hooks/useCollections.ts';
 import { useNfts } from '@hooks/useNfts.ts';
@@ -43,6 +39,14 @@ const MintNft = () => {
   const [imageCid, setImageCid] = useState<string | undefined>();
   const [imageSourceUrl, setImageSourceUrl] = useState<string>();
   const [mintPrice, setMintPrice] = useState<string>();
+
+  const [scanResult, setScanResult] = useState<string | null>(null);
+  const { ref } = useZxing({
+    onResult(result) {
+      setScanResult(result.getText());
+      nftReceiverRef.current.value = result.getText();
+    },
+  });
 
   const submitMintNft = useCallback(
     async (event: FormEvent) => {
@@ -108,15 +112,17 @@ const MintNft = () => {
               setImageCid={setImageCid}
             />
           </SGroup>
+          <SGroup>
+            <SLabel>Recipient Wallet QR Code Scanner</SLabel>
+            <video ref={ref} width="100%" />
+          </SGroup>
         </aside>
-
         <section>
           <SFormBlock>
             <SGroup>
               <SLabel>Name</SLabel>
               <FormControl type='text' ref={nftNameRef} placeholder='Enter NFT Name' required />
             </SGroup>
-
             <SGroup>
               <SLabel>Mint To</SLabel>
               <FormControl
@@ -126,14 +132,12 @@ const MintNft = () => {
                 defaultValue={activeAccount.address}
               />
             </SGroup>
-
             <SGroup>
               <SLabel>
                 Description <i>(optional)</i>
               </SLabel>
               <FormControl as='textarea' rows={3} ref={nftDescriptionRef} placeholder='Enter NFT Description' />
             </SGroup>
-
             {Array.isArray(ownedNftsFromAnotherCollection) && ownedNftsFromAnotherCollection.length > 0 && (
               <SGroup>
                 <SLabel>Select which NFT you want to use for the mint access</SLabel>
@@ -165,14 +169,12 @@ const MintNft = () => {
               restrictionType={RestrictionTypes.NFT_TAKEN}
             />
           </SFormBlock>
-
           <SInfoRow>
             <span>Mint Price</span>
             <span>
               {mintPrice} {api?.registry.chainTokens[0]}
             </span>
           </SInfoRow>
-
           <SPageControls>
             <Stack direction='horizontal' gap={3}>
               <Link to='..' className='w-50'>
@@ -180,7 +182,6 @@ const MintNft = () => {
                   Back
                 </ActionButton>
               </Link>
-
               <ActionButton type='submit' disabled={!isEligibleToMint} className='secondary w-50'>
                 Create NFT
               </ActionButton>
